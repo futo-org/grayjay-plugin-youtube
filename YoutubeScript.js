15d3391a5d091405b0c9bd92ff87ebcf2f6944eb
Kelvin <kelvin@futo.org>
2025-02-01 23:46:03 +0100

    Remove incorrect warning

--- YoutubeScript.js
+++ YoutubeScript.js
@@ -2046,8 +2046,6 @@ function generateDash(parentSource, sourceObj, ustreamerConfig, abrUrl, itag) {
 		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"//"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
 	}, false, true);
 	if(!initialResp.isOk) {
-		if(_settings.useUMP)
-			bridge.toast("Please try disabling Force Experimental UMP setting, it is broken at the moment");
 		throw new ScriptException("Failed initial stream request [ " + initialResp.code + "]");
 	}
 
