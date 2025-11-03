700fb4364ceb2cae2cdb88a27d8d4addca1cc735
Danilo <danilo.ti@hotmail.com>
2025-11-03 06:15:52 -0500

    Removing comment

--- YoutubeScript.js
+++ YoutubeScript.js
@@ -4234,7 +4234,7 @@ class YTABRExecutor {
 		let initialHeaders = {
 			"Origin": "https://www.youtube.com",
 			"Accept": "*/*",
-			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"//"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 		}
 		if (useLogin) {
 			initialHeaders = getAuthContextHeaders(false);
